9eb687ba8032c9428db7d8e36db6d58205565f62
Ferna <fernandoschimidt@outlook.com>
2024-07-19 12:27:32 -0300

    tela de registro de ponto
    Validação no botão de registro de ponto, exibição na tela dos últimos dois registros de ponto do dia

--- ponto-horarios/src/app/components/registrar-ponto/registrar-ponto.component.ts
+++ ponto-horarios/src/app/components/registrar-ponto/registrar-ponto.component.ts
@@ -1,30 +1,55 @@
 import { Component } from '@angular/core';
 import { RegistroPontoService } from '../../services/registro-ponto.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-registrar-ponto',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, NgClass],
   templateUrl: './registrar-ponto.component.html',
   styleUrl: './registrar-ponto.component.scss',
 })
 export class RegistrarPontoComponent {
+  isPressed = false;
+  errorMessage = '';
+  pressTimer: any;
   ultimosRegistros: any[] = [];
   funcionarioId = 1; // substituir com o ID real do funcionário
 
-  constructor(private registroPontoService: RegistroPontoService) {}
+  constructor(private registroPontoService: RegistroPontoService) {
+    this.carregarUltimosRegistros();
+  }
+
+  startPress() {
+    this.isPressed = true;
+    this.pressTimer = setTimeout(() => {
+      // Lógica para registrar o ponto após 2 segundos
+      this.registrarPonto();
+      this.isPressed = false;
+    }, 1000);
+  }
+  endPress() {
+    this.isPressed = false;
+    clearTimeout(this.pressTimer);
+  }
 
   registrarPonto() {
-    this.registroPontoService.registrarPonto().subscribe((response) => {
-      console.log('Ponto registrado', response);
-    });
+    this.registroPontoService.registrarPonto().subscribe(
+      (response) => {
+        console.log('Ponto registrado', response);
+        this.carregarUltimosRegistros();
+      },
+      (error) => {
+        console.log(error.error.message);
+      }
+    );
   }
 
   carregarUltimosRegistros() {
     this.registroPontoService
       .getUltimosRegistrosDoDia(this.funcionarioId)
       .subscribe((data) => {
+        console.log(data);
         this.ultimosRegistros = data;
       });
   }
